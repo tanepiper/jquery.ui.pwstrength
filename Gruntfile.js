b523de34934d5b83bf1ede52913a0439ba8379b0
Alejandro Blanco <alejandro.b.e@gmail.com>
2013-12-25 21:20:02 +0100

    Use grunt to build the plugin
    Generate a minified version, and a source map for it.

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+/*jslint node: true */
+/*global */
+
+module.exports = function (grunt) {
+    "use strict";
+
+    // Project configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jslint: { // configure the task
+            client: {
+                src: [
+                    'src/*.js'
+                ],
+                directives: {
+                    browser: true,
+                    predef: [
+                        'jQuery'
+                    ]
+                }
+            }
+        },
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['src/*.js'],
+                dest: '<%= pkg.name %>-<%= pkg.version %>.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/* <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+                sourceMap: '<%= pkg.name %>-<%= pkg.version %>.min.map'
+            },
+            dist: {
+                files: {
+                    '<%= pkg.name %>-<%= pkg.version %>.min.js': ['<%= concat.dist.dest %>']
+                }
+            }
+        },
+        shell: {
+            makeDir: {
+                command: 'mkdir -p dist'
+            },
+            moveFiles: {
+                command: 'mv <%= pkg.name %>-<%= pkg.version %>* dist/'
+            }
+        }
+    });
+
+    // Load the plugins
+    grunt.loadNpmTasks('grunt-jslint');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    // Default task(s)
+    grunt.registerTask('default', ['jslint', 'concat', 'uglify', 'shell']);
+};
